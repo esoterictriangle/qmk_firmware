d0517554acd60b72b44bbbe2336dc662c866d000
Esoteric Triangle <triangularart@gmail.com>
2020-01-17 10:18:36 -0600

    added colemak layer and a DF call inside _FN1; also changed MO to TT with a toggle of 2 taps

--- keyboards/keebio/quefrency/keymaps/esoterictriangle/keymap.c
+++ keyboards/keebio/quefrency/keymaps/esoterictriangle/keymap.c
@@ -7,13 +7,15 @@ extern keymap_config_t keymap_config;
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
 #define _BASE 0
-#define _FN1 1
+#define _COLEMAK 1
+#define _FN1 2
 #define KC_COPY LCTL(KC_C)
 #define KC_PASTE LCTL(KC_V)
 #define KC_UNDO LCTL(KC_Z)
 #define KC_NTAB LCTL(KC_T)
 #define KC_KEE LCTL(LALT(KC_A))
 #define KC_TASK LCTL(LSFT(KC_ESC))
+#define TAPPING_TOGGLE 2
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
@@ -31,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	* |-----|-----|   +-------+-+---+-+---+-+---+-+---+-+---+-+---+-+---+-----+-+---------------+---+-----------+
 	* | KEE |UNDO |   |   LSFT   |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |COMM | DOT |SLSH | RSFT | UP  |PGDN |
 	* |-----|-----|   +---------+-----+-----+-----+-----+-+---+-----+-----------+-------------------------------+
-	* |COPY |PASTE|   |RCTL |LGUI |LALT |MO(1)|   SPACE   |       SPACE     |MO(1)|RALT |RCTL |LEFT |DOWN |RGHT |
+	* |COPY |PASTE|   |RCTL |LGUI |LALT |TT(2)|   SPACE   |       SPACE     |TT(2)|RALT |RCTL |LEFT |DOWN |RGHT |
 	* +-----------+   +-----------------------------------+-----------------------------------------------+-----+
 	*/
 	
@@ -40,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_HOME,   KC_END,   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_PSCR, \
     KC_NTAB,   KC_F5,   KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_PGUP, \
     KC_KEE,   KC_UNDO,   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN, \
-    KC_COPY,   KC_PASTE,  KC_LCTL, KC_LGUI, KC_LALT, MO(_FN1), KC_SPC,        KC_SPC, XXXXXXX, MO(_FN1), KC_RALT, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+    KC_COPY,   KC_PASTE,  KC_LCTL, KC_LGUI, KC_LALT, TT(_FN1), KC_SPC,        KC_SPC, XXXXXXX, TT(_FN1), KC_RALT, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
 	/*
@@ -53,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	* |-----|-----|   +-------+-+---+-+---+-+---+-+---+-+---+-+---+-+---+-----+-+---------------+---+-----------+
 	* |     |     |   |          |     |     |     |     |     | KP1 | KP2 | KP3 |     |     |      |     |     |
 	* |-----|-----|   +---------+-----+-----+-----+-----+-+---+-----+-----------+-------------------------------+
-	* |     |     |   |     |     |     |     |           |                 |     |     |     |     |     |     |
+	* |DF(1)|     |   |     |     |     |     |           |                 |     |     |     |     |     |     |
 	* +-----------+   +-----------------------------------+-----------------------------------------------+-----+
 	*/
 	
@@ -62,6 +64,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_VOLU, KC_VOLD, _______, _______, KC_UP, _______,  _______, _______, KC_KP_7, KC_KP_8, KC_KP_9, _______, _______, _______, _______, _______, _______, \
     KC_CALC, _______, _______,  KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, KC_KP_4, KC_KP_5, KC_KP_6, _______, _______, _______, _______, _______, \
     _______, _______, _______, _______, _______, _______, _______, _______, KC_KP_1, KC_KP_2, KC_KP_3, _______, _______, _______, _______, _______, \
-    _______, _______, _______, _______, _______, _______, _______,      KC_KP_0, XXXXXXX, _______, _______, _______, _______, _______, _______
+    DF(_COLEMAK), _______, _______, _______, _______, _______, _______,      KC_KP_0, XXXXXXX, _______, _______, _______, _______, _______, _______
+  ),
+  
+  /*
+	* +-----------+   +-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----------+-----------+-----+
+	* | ESC |TASK |   | GRV |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |MINS | EQL |BSPC | DEL |
+	* |-----|-----|   +-----------------------------------------------------------------------------------------+
+	* |HOME | END |   | TAB |  Q  |  W  |  F  |  P  |  G  |  J  |  L  |  U  |  Y  | SCLN|LBRC |RBRC |BSLS |PSCR |
+	* |-----|-----|   +-----+-+---+-+---+-+---+-+---+-+---+-+---+-+---+-+---+-+---+-------------+---------------+
+	* |NTAB | F5  |   | CAPS  |  A  |  R  |  S  |  T  |  D  |  H  |  N  |  E  |  I  |  O  |QUOT |   ENT   |PGUP |
+	* |-----|-----|   +-------+-+---+-+---+-+---+-+---+-+---+-+---+-+---+-----+-+---------------+---+-----------+
+	* | KEE |UNDO |   |   LSFT   |  Z  |  X  |  C  |  V  |  B  |  K  |  M  |COMM | DOT |SLSH | RSFT | UP  |PGDN |
+	* |-----|-----|   +---------+-----+-----+-----+-----+-+---+-----+-----------+-------------------------------+
+	* |COPY |PASTE|   |RCTL |LGUI |LALT |TT(2)|   SPACE   |       SPACE     |TT(2)|RALT |RCTL |LEFT |DOWN |RGHT |
+	* +-----------+   +-----------------------------------+-----------------------------------------------+-----+
+	*/
+  
+  [_COLEMAK] = LAYOUT_65_with_macro(
+    KC_ESC,   KC_TASK,   KC_GRV, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL, XXXXXXX, KC_BSPC,  KC_DEL, \
+    KC_HOME,   KC_END,   KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,    KC_LBRC, KC_RBRC, KC_BSLS, KC_PSCR, \
+    KC_NTAB,   KC_F5,   KC_CAPS, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O, KC_QUOT, KC_ENT,  KC_PGUP, \
+    KC_KEE,   KC_UNDO,   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN, \
+    KC_COPY,   KC_PASTE,  KC_LCTL, KC_LGUI, KC_LALT, MO(_FN1), KC_SPC,        KC_SPC, XXXXXXX, MO(_FN1), KC_RALT, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
   )
 };
