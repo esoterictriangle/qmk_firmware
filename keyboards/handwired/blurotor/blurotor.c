ed2512fdc0f7fa13cb9490e89f1dc3b6d1713fc9
Esoteric Triangle <triangularart@gmail.com>
2020-08-13 08:05:56 -0500

    added numlock led support

--- keyboards/handwired/blurotor/blurotor.c
+++ keyboards/handwired/blurotor/blurotor.c
@@ -15,3 +15,16 @@
  */
 
 #include "blurotor.h"
+
+bool led_update_kb(led_t led_state) {
+    bool res = led_update_user(led_state);
+    if(res) {
+        // writePin sets the pin high for 1 and low for 0.
+        // In this example the pins are inverted, setting
+        // it low/0 turns it on, and high/1 turns the LED off.
+        // This behavior depends on whether the LED is between the pin
+        // and VCC or the pin and GND.
+        writePin(CAPS_LOCK_LED_PIN, !led_state.num_lock);
+    }
+    return res;
+}
