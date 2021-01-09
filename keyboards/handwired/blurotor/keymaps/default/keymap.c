c29e9d4a1dbb7292fda9243643d3f1571496a110
esoteric triangle <triangularart@gmail.com>
2021-01-09 09:27:54 -0600

    fixed encoder code; caught missed commas in layout

--- keyboards/handwired/blurotor/keymaps/default/keymap.c
+++ keyboards/handwired/blurotor/keymaps/default/keymap.c
@@ -20,16 +20,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
         KC_PSLS,   KC_PAST,    KC_PMNS,    KC_NLCK,
         KC_KP_7,   KC_KP_8,    KC_KP_9,    KC_PPLS,
-        KC_KP_4    KC_KP_5,    KC_KP_6,
-        KC_KP_1    KC_KP_2,    KC_KP_3,    KC_PENT,
-        KC_KP_0,               KC_PDOT,
+        KC_KP_4,   KC_KP_5,    KC_KP_6,
+        KC_KP_1,   KC_KP_2,    KC_KP_3,    KC_PENT,
+        KC_KP_0,               KC_PDOT
     ),
 };
 
 void encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == 0) {
         if (clockwise) {
             tap_code(KC_VOLU);
         } else {
             tap_code(KC_VOLD);
         }
+}
