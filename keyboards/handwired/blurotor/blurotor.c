addd39fc9b7a68db93bc59a6d753c05f8a799d44
esoteric triangle <triangularart@gmail.com>
2021-01-10 15:34:01 -0600

    added code from a fix to another keeb's LED issues this method works, but I wish I knew why the basic define doesn't.

--- keyboards/handwired/blurotor/blurotor.c
+++ keyboards/handwired/blurotor/blurotor.c
@@ -16,16 +16,18 @@
 
 #include "blurotor.h"
 
-/* (bool led_update_kb(led_t led_state) {
-    bool res = led_update_user(led_state);
-    if(res) {
-        // writePin sets the pin high for 1 and low for 0.
-        // In this example the pins are inverted, setting
-        // it low/0 turns it on, and high/1 turns the LED off.
-        // This behavior depends on whether the LED is between the pin
-        // and VCC or the pin and GND.
-        writePin(C6, !led_state.num_lock);
-    }
-    return res;
+void matrix_init_kb(void) {
+  setPinOutput(C6);
+  matrix_init_user();
 }
-*/
+
+void led_set_kb(uint8_t usb_led) {
+	// put your keyboard LED indicator (ex: Caps Lock LED) toggling code here
+  if (IS_LED_ON(usb_led, USB_LED_NUM_LOCK)) {
+    writePinLow(C6);
+  } else {
+    writePinHigh(C6);
+  }
+
+	led_set_user(usb_led);
+}
